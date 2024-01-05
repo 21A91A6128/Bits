14e873ce1c787a05986a767358737bffe0752b24
RYALI JAYA SAI SRI VARDHAN <97591375+21A91A6128@users.noreply.github.com>
2024-01-05 18:52:34 +0530

    Create Bitwise Operations within range.py

---
+++ Bitwise Operations within range.py
@@ -0,0 +1,22 @@
+#and,xor,or for given array within limits
+a=list(map(int,input().split()))
+n=int(math.log2(max(a)))
+b=[]
+for i in range(len(a)):
+    b.append([])
+for j in range(n,-1,-1):
+    c=0
+    for i in range(len(a)):
+        if((a[i]>>j)&1):
+            c+=1
+        b[i].append(c)
+print(b)
+l,r=map(int,input().split())
+c=0
+for i in range(len(b[r])):
+    #and
+    if b[r][i]==r+1:
+        c+=(2**(n-i))
+    #or b[r][i]>=1
+    #xor b[r][i]%2
+print(c)
